ba52e6a469ad274375fc6613660434f5baf9833e
Claudia <claudiatcruz2002@gmail.com>
2022-04-30 16:19:47 +0200

    Implementado método Hit.

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -75,6 +75,7 @@ public class Deck : MonoBehaviour
             /*TODO:
              * Si alguno de los dos obtiene Blackjack, termina el juego y mostramos mensaje
              */
+            Debug.Log(dealer.GetComponent<CardHand>().points);
             //Si el dealer obtiene blackjack o el jugador obtiene mas de 21 puntos
             if(dealer.GetComponent<CardHand>().points == 21 || player.GetComponent<CardHand>().points > 21){
                 //dar la vuelta a la carta del dealer
@@ -85,6 +86,8 @@ public class Deck : MonoBehaviour
                 } else{
                     finalMessage.text = "Has perdido";
                 }
+                stickButton.interactable = false;
+                hitButton.interactable = false;
             }
             //si el player obtiene blackjack o el dealer obtiene mas de 21 puntos
             else if(player.GetComponent<CardHand>().points == 21 || dealer.GetComponent<CardHand>().points > 21){
@@ -96,6 +99,8 @@ public class Deck : MonoBehaviour
                 } else{
                     finalMessage.text = "Has ganado";
                 }
+                stickButton.interactable = false;
+                hitButton.interactable = false;
             }
         }
     }
@@ -134,14 +139,29 @@ public class Deck : MonoBehaviour
         /*TODO: 
          * Si estamos en la mano inicial, debemos voltear la primera carta del dealer.
          */
-        
+        if(dealer.GetComponent<CardHand>().cards.Count == 2){
+            dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
+        }
         //Repartimos carta al jugador
         PushPlayer();
 
         /*TODO:
          * Comprobamos si el jugador ya ha perdido y mostramos mensaje
          */      
-
+        Debug.Log(dealer.GetComponent<CardHand>().points);
+        //Si el dealer obtiene blackjack o el jugador obtiene mas de 21 puntos
+        if(dealer.GetComponent<CardHand>().points == 21 || player.GetComponent<CardHand>().points > 21){
+            //dar la vuelta a la carta del dealer
+            dealer.GetComponent<CardHand>().cards[0].GetComponent<CardModel>().ToggleFace(true);
+            //mostrar mensaje
+            if(dealer.GetComponent<CardHand>().points == 21){
+                finalMessage.text = "Blackjack del dealer. Has perdido";
+            } else{
+                finalMessage.text = "Has perdido";
+            }
+            stickButton.interactable = false;
+            hitButton.interactable = false;
+        }
     }
 
     public void Stand()
